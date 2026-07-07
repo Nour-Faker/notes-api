906dd33f981c42e1cce05fef01585f96888bb73d
Nour-Faker <nour.feker2004@gmail.com>
2026-07-07 11:09:38 +0100

    feat: add Alembic migrations, add is_pinned to notes

--- app/models/note.py
+++ app/models/note.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey,Boolean
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.config.database import Base
@@ -13,5 +13,6 @@ class Note(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     owner_id   = Column(Integer, ForeignKey("users.id"), nullable=False)
+    is_pinned = Column(Boolean, default=False)
 
     owner = relationship("User", back_populates="notes")
